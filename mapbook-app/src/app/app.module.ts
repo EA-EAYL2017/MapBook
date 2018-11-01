112a10b8c324029bf296eea0b7cb80f313ed4eb0
Michael <michaeld@kainos.com>
2018-11-01 11:45:02 +0000

    Fixed up imports

--- mapbook-app/src/app/app.module.ts
+++ mapbook-app/src/app/app.module.ts
@@ -1,36 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
-
-=======
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> c93de6e06b2bcaae5a9cf247d42fcb633792865e
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { DetailComponent } from './detail/detail.component';
 import { NameFilterPipe } from './name-filter.pipe';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    DetailComponent
+    DetailComponent,
     NameFilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule
-=======
     NgbModule.forRoot(),
     FormsModule,
     HttpClientModule
->>>>>>> c93de6e06b2bcaae5a9cf247d42fcb633792865e
   ],
   providers: [],
   bootstrap: [AppComponent]
