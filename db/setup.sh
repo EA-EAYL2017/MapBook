f6e40b5a0593fe33449a6e97e828f83f2dca0bd9
owenatkinson <owena@kainos.com>
2018-11-01 13:47:53 +0000

    User Story 3, db fields and sample data added

--- db/setup.sh
+++ db/setup.sh
@@ -1,8 +1,9 @@
 if [[ $* == *--seed* ]]; then
-	sudo mysql -e "drop database MapBook;"
+	sudo mysql -e "drop database if exists MapBook;"
 fi
 
 sudo mysql -e "set @username='${DB_USER}';set @password='${DB_PASS}';source 01-db-setup.sql;"
+sudo mysql -e "set @username='${DB_USER}';set @password='${DB_PASS}';source 02-details-fields.sql;"
 echo DB Setup Complete!
 
 if [[ $* == *--seed* ]]; then
