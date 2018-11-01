b09998ce474a44a8205aab0341a471f6433458b1
Peter Gordon <p.gordon@kainos.com>
2018-11-01 11:00:25 +0000

    Initial DB structure

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# kAssist
+# MapBook
+Used to allow employees to book MAP training courses.
+
+## DB Setup
+Export the DB connection parameters (application DB user and password):
+
+```
+export DB_USER=map_book
+export DB_PASS=password
+cd db
+./setup.sh
+```
+
+## Running the application
+### Backend (express)
+Export the DB connection parameters (application DB user and password) and start the Node application. It will run on http://localhost:8002.
+
+```
+cd express
+npm install
+export DB_USER=map_book
+export DB_PASS=password
+nodemon index.js
+```
+
+### Frontend (angular)
+```
+cd mapbook-app
+npm install
+npm start
+```
+This will start the frontend on http://localhost:8001.
--- db/01-db-setup.sql
+++ db/01-db-setup.sql
@@ -1,4 +1,4 @@
--- set @username := 'employee_manager';
+-- set @username := 'map_book';
 -- set @password := 'password';
 
 -- make sure the running user has these permissions (to allow granting)
@@ -8,8 +8,8 @@
 -- show grants;
 
 -- Ensure clean state each time
-CREATE DATABASE IF NOT EXISTS EmployeeManager;
-USE EmployeeManager;
+CREATE DATABASE IF NOT EXISTS MapBook;
+USE MapBook;
 SET @query1 = CONCAT('DROP USER IF EXISTS "',@username,'"@"localhost"');
 PREPARE stmt FROM @query1;
 EXECUTE stmt;
@@ -22,7 +22,7 @@ EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 SET @query1 = CONCAT(
-	'GRANT SELECT,INSERT ON EmployeeManager.* TO "',@username,
+	'GRANT SELECT,INSERT ON MapBook.* TO "',@username,
     '"@"localhost" IDENTIFIED BY "',@password,'"'
 );
 PREPARE stmt FROM @query1;
@@ -30,11 +30,21 @@ EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
 
 -- Tables
-CREATE TABLE IF NOT EXISTS Employees (
+CREATE TABLE IF NOT EXISTS Courses (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(30),
-    address VARCHAR(90),
-    national_insurance VARCHAR(13),
-    bic_iban CHAR(22),
-    salary DECIMAL(11, 2)
+	title VARCHAR(45),
+    date DATETIME,
+    location VARCHAR(30)
 );
+
+-- Sample data
+INSERT INTO Courses VALUES ("Apprentice Development Programme", '2018-12-01 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Engineering Academy", '2018-12-18 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Consultant Development Programme", '2018-12-02 09:00:00', "Castleward");
+INSERT INTO Courses VALUES ("Management Development Programme", '2019-01-10 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Kainos Principals Programme", '2018-12-05 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Sales Development Programme", '2018-12-15 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Kimble Training", '2019-06-21 09:00:00', "Belfast");
+INSERT INTO Courses VALUES ("Introduction to Serverless", '2019-07-25 09:00:00', "Gdansk");
+INSERT INTO Courses VALUES ("Crucial Conversations", '2019-02-11 09:00:00', "London");
+INSERT INTO Courses VALUES ("Presales Orientation", '2018-12-01 09:00:00', "Online");
