6db0dea8b0d9dc47cb88d5426447e43c05591a48
Peter Lappin <peterl@kainos.com>
2018-11-01 10:54:09 +0000

    added getCourses method to data service

--- mapbook-app/src/app/data.service.ts
+++ mapbook-app/src/app/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Course } from './course';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  courses;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getCourses(): void{
+    this.http.get<Course[]>('/courses')
+    .subscribe(data => {
+      this.courses = data;
+    })
+  }
 }
