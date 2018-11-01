d063fcc6d15bfef80487f6b480a2bd4af351f49e
Michael <michaeld@kainos.com>
2018-11-01 13:04:28 +0000

    More merge stuf

--- mapbook-app/src/app/data.service.ts
+++ mapbook-app/src/app/data.service.ts
@@ -6,17 +6,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DataService {
-<<<<<<< HEAD
-  courses = [
-    {
-      id: 1,
-      name: 'Training',
-      date: '23/04/18',
-      location: 'Belfast'
-    }
-  ];
-=======
->>>>>>> d3da73cf74b9a60c6a03610d233989708bf4a356
   constructor(private http: HttpClient) { }
 
   courses;
@@ -26,7 +15,7 @@ export class DataService {
     .subscribe(data => {
       this.courses = data;
       console.log(this.courses);
-    })
+    });
   }
 
   public deleteCourse(oldCourse: Course): void {
