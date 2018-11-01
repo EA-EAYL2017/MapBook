51656c40bd44cbab3e2ed4581b79d2aad36fcfbd
Michael <michaeld@kainos.com>
2018-11-01 11:12:48 +0000

    Changed app component

--- mapbook-app/src/app/app.module.ts
+++ mapbook-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
