7239f7f4aff489e1721aa52e6a8e74bc2892cf40
Peter Lappin <peterl@kainos.com>
2018-11-01 11:39:22 +0000

    added proxy config

---
+++ mapbook-app/src/app/name-filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Course } from './course';
+
+@Pipe({
+  name: 'nameFilter'
+})
+export class NameFilterPipe implements PipeTransform {
+
+  transform(courses: Course[], filterText: string): Course[] {
+    if(!courses) return [];
+    if(!filterText) return courses;
+
+    return courses.filter(c => {
+      return c.name.toLowerCase()
+      .includes(filterText.toLowerCase());
+    });
+  }
+
+}
