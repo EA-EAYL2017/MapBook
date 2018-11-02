8cbc35be663b540e5d329503522e1d5d8e48afd6
Peter Lappin <peterl@kainos.com>
2018-11-02 13:02:44 +0000

    Added tests for 003

--- mapbook-app/specs/detail.component.spec.ts
+++ mapbook-app/specs/detail.component.spec.ts
@@ -8,6 +8,17 @@ describe('DetailComponent', () => {
   it('should have update button', () => {
     browser.get('http://localhost:8001');
     element(by.id('item')).click();
-    expect(element(by.id('update')).getText()).toEqual("Update");
+    expect(element(by.id('apply')).getText()).toEqual("Apply to Course");
   }); 
+
+  it('Should have headers', () => {
+    browser.get('http://localhost:8001');
+    expect(element(by.id('detail')).getText()).toContain("Name:");
+    expect(element(by.id('detail')).getText()).toContain("Date:");
+    expect(element(by.id('detail')).getText()).toContain("Location:");
+    expect(element(by.id('detail')).getText()).toContain("Description:");
+    expect(element(by.id('detail')).getText()).toContain("Target Audience:");
+    expect(element(by.id('detail')).getText()).toContain("Duration:");
+    expect(element(by.id('detail')).getText()).toContain("Trainer:");
+  })
 });
