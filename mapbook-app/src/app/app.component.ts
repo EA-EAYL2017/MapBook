8c0918ee39e89f9b8be63dfe5d28d5f625a5495a
Michael Dunwoody <michaeld@kainos.com>
2018-11-02 15:08:41 +0000

    Peter and I got the site flow to work

--- mapbook-app/src/app/app.component.ts
+++ mapbook-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgbModule, NgbTabset } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbTabset, NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 import { AddEmployeeToCourseComponent } from './add-employee-to-course/add-employee-to-course.component';
 
 @Component({
@@ -13,14 +13,26 @@ export class AppComponent {
   @ViewChild('tabs')
   private tabs: NgbTabset;
   addEmployee: AddEmployeeToCourseComponent;
+  tabChangeAllowed: number;
 
   constructor(addEmployee: AddEmployeeToCourseComponent){
     this.addEmployee = addEmployee;
+    this.tabChangeAllowed = 0;
   }
 
   applyToJoin(selectedCourseId: number) {
     console.log('got clicked    ' + selectedCourseId);
+    this.tabChangeAllowed = 1;
     this.addEmployee.sendData(selectedCourseId);
-    this.tabs.select('ngb-tab-1');
+    this.tabs.select('tab2');
+    this.tabChangeAllowed = 0;
+  }
+
+  public beforeChange($event: NgbTabChangeEvent) {
+    if(this.tabChangeAllowed == 0){
+      if($event.nextId === 'tab2') {
+        $event.preventDefault();
+      }
+    }
   }
 }
