ee8e95f63d8c38337ff6fb3d96a0b6fb6ca1a5c2
Peter Lappin <peterl@kainos.com>
2018-11-02 13:25:42 +0000

    added negative test for filter

--- mapbook-app/specs/name-filter.pipe.spec.ts
+++ mapbook-app/specs/name-filter.pipe.spec.ts
@@ -2,9 +2,15 @@ import { NameFilterPipe } from '../src/app/name-filter.pipe';
 import { browser, element, by } from 'protractor';
 
 describe('NameFilterPipe', () => {
-  it('create an instance', () => {
+  it('should find a result', () => {
     browser.get('http://localhost:8001');
     element(by.id('filter')).sendKeys('App');
     expect(element(by.id('item')).getText()).toContain("Apprentice");
   });
+
+  it('shouldnt find result for negative search term', () => {
+    browser.get('http://localhost:8001');
+    element(by.id('filter')).sendKeys('Awfwfwef');
+    expect(element(by.id('item'))).toBeUndefined();
+  })
 });
